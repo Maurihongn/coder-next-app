d3c809bbd184bb299d81eabf2c04a3571067ce32
Maurihongn <107648425+Maurihongn@users.noreply.github.com>
2023-11-15 07:27:35 -0300

    firebase, rutas admin

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCppBFJRQ0cS2k-zAnrxlxzFtGMYPUl9CU',
+  authDomain: 'ecommercecoderhouse-1b691.firebaseapp.com',
+  projectId: 'ecommercecoderhouse-1b691',
+  storageBucket: 'ecommercecoderhouse-1b691.appspot.com',
+  messagingSenderId: '8291502807',
+  appId: '1:8291502807:web:3a5148f0009ac47a737e92',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
