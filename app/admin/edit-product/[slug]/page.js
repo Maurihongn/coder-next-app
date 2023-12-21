c8a506824ddd87bceffbcb523705eb1c0600cb5a
Maurihongn <107648425+Maurihongn@users.noreply.github.com>
2023-12-21 19:49:04 -0300

    error

--- app/admin/edit-product/[slug]/page.js
+++ app/admin/edit-product/[slug]/page.js
@@ -9,6 +9,10 @@ const getProduct = async (slug) => {
       }
     );
 
+    if (!response.ok) {
+      throw new Error('Error fetching data');
+    }
+
     const data = await response.json();
 
     return data;
--- app/components/catalog/ProductList.js
+++ app/components/catalog/ProductList.js
@@ -9,6 +9,10 @@ const getItemsByCategory = async (category) => {
       }
     );
 
+    if (!response.ok) {
+      throw new Error('Error fetching data');
+    }
+
     const data = await response.json();
 
     return data;
